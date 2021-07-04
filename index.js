0d7437c06c9c9b255b14941892fd6a1bbfb1421d
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-05 01:40:31 +0600

    added books to db

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
-const app = express()
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
 
 const port = 5000
 
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mps7w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,7 +18,7 @@ client.connect(err => {
         res.send('Hey there! its the server.')
     })
 
-    // to get the book list
+    // to get the book list from the server
     app.get('/books', (req, res) => {
         bookCollection.find({})
         .toArray((err, documents) => {
@@ -24,17 +27,15 @@ client.connect(err => {
     })
 
     // inserting many books
-    app.post('/addBooks', (req, res) => {
-        const books = req.body;
-        bookCollection.insertMany(books, (err, result) => {
-            console.log(err, result);
-            res.send({count: result})
-        })
-    })
+    // app.post('/addBooks', (req, res) => {
+    //     const books = req.body;
+    //     bookCollection.insertMany(books, (err, result) => {
+    //         console.log(err, result);
+    //         res.send({count: result})
+    //     })
+    // })
 
 
 });
-
-
 
 app.listen(port)
