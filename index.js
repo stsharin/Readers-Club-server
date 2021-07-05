e6d71fac470aa59a85c2994d1ac1eb372841a930
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-06 00:06:26 +0600

    added addbooks and customer details api

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 
+// port
 const port = 5000
 
+// middle wares
 app.use(cors())
 app.use(express.json())
 
+// db details and connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mps7w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const bookCollection = client.db("bookShop").collection("books");
+    const buyerCollection = client.db("bookShop").collection("buyers");
 
+    // default api
     app.get('/', (req, res) => {
         res.send('Hey there! its the server.')
     })
@@ -21,12 +26,37 @@ client.connect(err => {
     // to get the book list from the server
     app.get('/books', (req, res) => {
         bookCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
-    // inserting many books
+    // all book and buyer details for ADMIN
+    app.get('/customerDetails', (req, res) => {
+        buyerCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    // checkout list for specific users
+    app.get('/customerDetails/:email', (req, res) => {
+        const email = req.params.email;
+        buyerCollection.find({email: email})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    // add more books
+    app.get('/addBooks', (req, res) => {
+        bookCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    // only using this api to add books - fakeData
     // app.post('/addBooks', (req, res) => {
     //     const books = req.body;
     //     bookCollection.insertMany(books, (err, result) => {
